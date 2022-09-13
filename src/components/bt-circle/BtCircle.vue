fb3bf794b8cae6f3099bb85953d9405eea85a5c1
ilyasius1 <47688564+ilyasius1@users.noreply.github.com>
2022-09-13 19:44:28 +0300

    bets-circle

--- src/components/bt-circle/BtCircle.vue
+++ src/components/bt-circle/BtCircle.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="bt-circle">
-    <div class="bt-circle__percent-pink">32%</div>
+    <div class="bt-circle__percent-pink">{{ pink }}%</div>
     <div class="bt-circle__image">
+      <svg
+        class="bt-circle__progress-bar"
+        xmlns="http://www.w3.org/2000/svg"
+        :height="height"
+        :width="circleCenter * 2"
+        :viewBox="(0, 0, circleCenter * 2, height)"
+      >
+        <g fill="none" stroke="#2A2D2F">
+          <circle
+            :cx="circleCenter"
+            :cy="circleCenter"
+            :r="radius"
+            stroke="#fe6289"
+            :stroke-width="strokeWidth"
+            :stroke-dasharray="strokePink + ',' + strokeBlue + ',0,' + strokeNone"
+            :stroke-dashoffset="offsetPink"
+          />
+          <circle
+            :cx="circleCenter"
+            :cy="circleCenter"
+            :r="radius"
+            stroke="#08b0fb"
+            :stroke-width="strokeWidth"
+            :stroke-dasharray="strokeBlue + ',' + strokeNone + ',0,' + strokePink"
+            :stroke-dashoffset="offsetBlue"
+          />
+          <circle :cx="pointerX" :cy="pointerY" r="12.5" stroke="#08b0fb" fill="#08b0fb" />
+        </g>
+      </svg>
       <div class="bt-circle__content">
         <img src="../../assets/img/game/logo.png" alt="" />
         <p class="bt-circle__timer">00:30</p>
       </div>
     </div>
-    <div class="bt-circle__percent-blue">68%</div>
+    <div class="bt-circle__percent-blue">{{ 100 - pink }}%</div>
   </div>
 </template>
 <script src="./bt-circle.js"></script>
--- src/components/bt-circle/bt-circle.js
+++ src/components/bt-circle/bt-circle.js
@@ -1,3 +1,47 @@
 export default {
   name: 'BtCircle',
+  data() {
+    return {
+      pink: 50,
+      radius: 215,
+      pointerRadius: 13,
+      height: 373,
+      strokeWidth: 8,
+    };
+  },
+  computed: {
+    circleCenter() {
+      return this.radius + this.pointerRadius;
+    },
+    angleOffset() {
+      return Math.acos((this.height - this.circleCenter) / this.radius);
+    },
+    strokeFull() {
+      return this.radius * (2 * (Math.PI - this.angleOffset));
+    },
+    strokeBlue() {
+      return (this.strokeFull * (100 - this.pink)) / 100;
+    },
+    strokePink() {
+      return (this.strokeFull * this.pink) / 100;
+    },
+    strokeNone() {
+      return this.radius * 2 * this.angleOffset;
+    },
+    offsetPink() {
+      return this.radius * (-Math.PI / 2 - this.angleOffset);
+    },
+    offsetBlue() {
+      return this.offsetPink - this.strokePink;
+    },
+    angle() {
+      return Math.PI / 2 + this.angleOffset + ((Math.PI - this.angleOffset) * 2 * this.pink) / 100;
+    },
+    pointerX() {
+      return this.circleCenter + Math.cos(this.angle) * this.radius;
+    },
+    pointerY() {
+      return this.circleCenter + Math.sin(this.angle) * this.radius;
+    },
+  },
 };
