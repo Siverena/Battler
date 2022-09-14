5cfe1d3854483364d133dc08da9840d433c24bba
ilyasius1 <47688564+ilyasius1@users.noreply.github.com>
2022-09-14 11:58:57 +0300

    mm0

---
+++ src/components/bt-mobile-menu/BtMobileMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bt-mobile-menu"></div>
+  <div class="bt-mobile-menu__button"></div>
+  <div class="bt-mobile-menu__text"></div>
+  <div class="bt-mobile-menu__button bt-mobile-menu__button--chat"></div>
+</template>
+<script src="./bt-mobile-menu.js"></script>
