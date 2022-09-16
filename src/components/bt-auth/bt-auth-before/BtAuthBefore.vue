80c76f16d5f80af94c38390d07067bd81cd0f0e2
Siverena <vpupkin@company.com>
2022-09-16 19:55:32 +0300

    end

--- src/components/bt-auth/bt-auth-before/BtAuthBefore.vue
+++ src/components/bt-auth/bt-auth-before/BtAuthBefore.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="dl-auth-before__buttons">
-    <a @click="login" class="dl-auth-before__button"> Войти</a>
+  <div class="bt-auth-before">
+    <a class="bt-auth-before__text">Войти через:</a>
+    <div class="bt-auth-before__buttons">
+      <a @click="login" class="bt-auth-before__button bt-auth-before__button--vk">
+        <img src="@/assets/img/auth/vk.png" alt="" width="25" />
+      </a>
+      <a @click="login" class="bt-auth-before__button bt-auth-before__button--tel">
+        <img src="@/assets/img/chats/send.png" alt=""
+      /></a>
+    </div>
   </div>
 </template>
 <script src="./bt-auth-before.js"></script>
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: './build',
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/dotaloot_2022/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/battler/',
   devServer: {
     historyApiFallback: {
       rewrites: [{ from: /./, to: '/' }],
